58d6ab6e4cb8533a46242a7bf12c4bad6fd7ba76
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2023-06-29 12:32:22 +0200

    Lobby
    - Possibilité d'ajouter des utilisateurs dans le lobby
    - Retrait du nombre d'utilisateurs dans le local

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class Lobby : MonoBehaviour
 {
 
     [SerializeField] LocalNextButton localNextButton;
     [SerializeField] GameObject playerUIPrefab;
+    [SerializeField] GameObject gridLayout;
+    [SerializeField] Button playButton;
+    [SerializeField] Button removePlayerButton;
+    [SerializeField] Button addPlayerButton;
 
     string serverName;
     int playerNumber;
@@ -17,7 +22,7 @@ public class Lobby : MonoBehaviour
     int userStory;
 
     List<string> playersName;
-    List<GameObject> playersUI;
+    List<GameObject> playersUI = new List<GameObject>();
 
     [SerializeField] TMP_Text serverNameOut;
     [SerializeField] TMP_Text pokerPlanningOut;
@@ -32,7 +37,7 @@ public class Lobby : MonoBehaviour
     void Update()
     {
         this.serverName = localNextButton.GetServerName();
-        this.playerNumber = localNextButton.GetPlayerNumber();
+        this.playerNumber = playersUI.Count;
         this.pokerPlanning = localNextButton.GetPokerPlanning();
         this.difficulty = localNextButton.GetDifficulty();
         this.userStory = localNextButton.GetUserStory();
@@ -43,7 +48,17 @@ public class Lobby : MonoBehaviour
         this.userStoryOut.text = GetUserStory(this.userStory);
         this.playerNumberOut.text = this.playerNumber.ToString();
 
-        (this.playersName, this.playersUI) = CreatePlayers(this.playerNumber);
+        if (playersUI.Count <= 0){
+            removePlayerButton.interactable = false;
+            addPlayerButton.interactable = true;
+        } else if (playersUI.Count >= 9){
+            removePlayerButton.interactable = true;
+            addPlayerButton.interactable = false;
+        } else {
+            removePlayerButton.interactable = true;
+            addPlayerButton.interactable = true;
+        }
+        
     }
 
     (List<string>, List<GameObject>) CreatePlayers(int playerNumber){
@@ -86,5 +101,24 @@ public class Lobby : MonoBehaviour
             return "True";
         }
         return "False";
+    }
+
+    public void AddPlayer(){
+        GameObject player = Instantiate(playerUIPrefab) as GameObject;
+        player.transform.SetParent(gridLayout.transform);
+        playersUI.Add(player);
+
+        UIPlayer playerUI = player.GetComponent<UIPlayer>();
+        playerUI.text.text = "Player " + playersUI.Count + " :";
+        playerUI.inputField.placeholder.GetComponent<TMP_Text>().text = "Player " + playersUI.Count;
+    }
+
+    public void RemovePLayer(){
+        Destroy(playersUI.LastOrDefault());
+        playersUI.RemoveAt(playersUI.Count - 1);
+    }
+
+    public void LaunchGame(){
+        
     }
 }
--- Assets/Scripts/LocalNextButton.cs
+++ Assets/Scripts/LocalNextButton.cs
@@ -10,13 +10,11 @@ public class LocalNextButton : MonoBehaviour
 
     [SerializeField] Button nextButton;
     [SerializeField] TMP_InputField serverNameIn;
-    [SerializeField] TMP_InputField playerNumberIn;
     [SerializeField] Slider pokerPlanningIn;
     [SerializeField] TMP_Dropdown difficultyIn;
     [SerializeField] TMP_Dropdown userStoryIn;
 
     static string serverName;
-    static int playerNumber;
     static bool pokerPlanning;
     static int difficulty;
     static int userStory;
@@ -25,21 +23,13 @@ public class LocalNextButton : MonoBehaviour
         if (string.IsNullOrWhiteSpace(serverNameIn.text)) {
             return false;
         }
-        if (string.IsNullOrWhiteSpace(playerNumberIn.text)){
-            return false;
-        }
         if (userStoryIn.value < 0 | userStoryIn.value > 2){
-            return false;
-        }
-        playerNumber = Int32.Parse(playerNumberIn.text);
-        if (playerNumber < 1 || playerNumber > 9){
             return false;
         }
         serverName = serverNameIn.text;
         pokerPlanning = (Math.Round(pokerPlanningIn.value) == 1) ? true : false;
         difficulty = difficultyIn.value;
         userStory = userStoryIn.value;
-        // lobby = Lobby(serverName, playerNumber, pokerPlanning, difficulty, userStory);
         return true;
     }
 
@@ -61,9 +51,6 @@ public class LocalNextButton : MonoBehaviour
 
     public string GetServerName(){
         return serverName;
-    }
-    public int GetPlayerNumber(){
-        return playerNumber;
     }
     public bool GetPokerPlanning(){
         return pokerPlanning;
