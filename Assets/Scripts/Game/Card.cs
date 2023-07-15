683cbfdb39c7b2d0d5e06d5b37d18f7dd2cea17b
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2023-07-15 11:02:28 +0200

    Card
    - Rajout du type de carte
    - Enumération possible sur le type de carte

--- Assets/Scripts/Game/Card.cs
+++ Assets/Scripts/Game/Card.cs
@@ -1,35 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using UnityEngine;
 
 public class Card
 {
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TypeOfCard{
+        [EnumMember(Value = "Simple")]
+        Simple,
+        [EnumMember(Value = "Multiple")]
+        Multiple,
+        [EnumMember(Value = "Question")]
+        Question,
+        [EnumMember(Value = "RollTheDice")]
+        RollTheDice,
+        [EnumMember(Value = "Permanent")]
+        Permanent,
+        [EnumMember(Value = "Choice")]
+        Choice,
+        [EnumMember(Value = "Information")]
+        Information,
+    }
 
 
     public int id;
     public string category;
     public string description;
     public string result;
-    public bool permanent;
-    public bool question;
+    public TypeOfCard typeOfCard;
 
-    public Card(int id, string category, string description, string result, bool permanent, bool question)
+    public Card(int id, string category, string description, string result, TypeOfCard typeOfCard)
     {
         this.id = id;
         this.category = category;
         this.description = description;
         this.result = result;
-        this.permanent = permanent;
-        this.question = question;
+        this.typeOfCard = typeOfCard;
     }
 
     public override string ToString()
     {
         return  "id : " + this.id +
-                ", category : " + this.category +
-                ", result : " + this.result +
-                ", description : " + this.description +
-                ", permanent : " + this.permanent + 
-                ", question : " + this.question;
+                ", category : " + this.category + $"\n" +
+                ", result : " + this.result + $"\n" +
+                ", description : " + this.description + $"\n" +
+                ", type : " + this.typeOfCard.ToString();
     }
 }
