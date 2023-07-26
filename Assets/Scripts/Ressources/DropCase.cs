52550f3ec238ea1c7bbfdf0ee5960fc70d914a03
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2023-07-26 07:28:37 +0200

    TDDD
    - Les US se déplacent dans le content en fonction des entrées de US
    - Elles avancent pour faire de la place si une US passe au-dessus, et elles reculent quand une US quitte leur case
    - Update permanent pour voir s'il faut rajouter des cases vides ou en supprimer

--- Assets/Scripts/Ressources/DropCase.cs
+++ Assets/Scripts/Ressources/DropCase.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DropCase : MonoBehaviour, IDropHandler
+public class DropCase : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [HideInInspector] public UserStoryUI userStoryUI;
     [HideInInspector] public DropContent dropContent;
+    public DropCase previousDropCase;
+    public DropCase nextDropCase;
     
     public void OnDrop(PointerEventData eventData)
     {
@@ -33,5 +35,66 @@ public class DropCase : MonoBehaviour, IDropHandler
             userStoryUI.transform.SetParent(this.gameObject.transform);
             this.userStoryUI = userStoryUI.GetComponent<UserStoryUI>();
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag is null){
+                return;
+            }
+        
+        if (eventData.pointerDrag.GetComponent<DraggableItem>() is null){
+                return;
+            }
+        if (this.transform.childCount > 0){
+            if(nextDropCase is null){
+                return;
+            }
+            this.nextDropCase.MoveToNext(this.transform.GetChild(0), this.userStoryUI);
+            this.userStoryUI = null;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag is null){
+                return;
+            }
+        
+        if (eventData.pointerDrag.GetComponent<DraggableItem>() is null){
+                return;
+            }
+        if (this.nextDropCase is null){
+            return;
+        }
+        if (this.transform.childCount == 0){
+            this.nextDropCase.MoveToPrev();
+        }
+    }
+    public void MoveToNext(Transform card, UserStoryUI userStory){
+        if(this.transform.childCount == 0){
+            card.SetParent(this.transform);
+            this.userStoryUI = userStory;
+            return;
+        }
+        if(this.nextDropCase is null){
+        } else {
+            this.nextDropCase.MoveToNext(this.transform.GetChild(0), this.userStoryUI);
+            card.SetParent(this.transform);
+            this.userStoryUI = userStory;
+        }
+    }
+    public void MoveToPrev(){
+        if(this.transform.childCount == 0){
+            this.previousDropCase.userStoryUI = null;
+            return;
+        }
+        this.transform.GetChild(0).transform.SetParent(this.previousDropCase.transform);
+        this.previousDropCase.userStoryUI = this.userStoryUI;
+        if(this.nextDropCase is null){
+            this.userStoryUI = null;
+            return;
+        }
+        this.nextDropCase.MoveToPrev();
     }
 }
--- Assets/Scripts/Ressources/DropContent.cs
+++ Assets/Scripts/Ressources/DropContent.cs
@@ -18,8 +18,38 @@ public class DropContent : MonoBehaviour, IDropHandler
             go.AddComponent<DropCase>();
             go.GetComponent<DropCase>().dropContent = this;
             cases.Add(go.GetComponent<DropCase>());
+            if(i == 0){
+                go.GetComponent<DropCase>().previousDropCase = null;
+            } else {
+                go.GetComponent<DropCase>().previousDropCase = cases[i-1];
+                cases[i-1].nextDropCase = go.GetComponent<DropCase>();
+                if (i == 5)
+                    go.GetComponent<DropCase>().nextDropCase = null;
+            }
         }
     }
+
+    void Update(){
+        int filledCases = 0;
+        for (int i = cases.Count-1; i >= 6; i--){
+            if(cases[i].transform.childCount == 0){
+                if(i % 2 == 0){
+                    if(cases[i+1].transform.childCount == 0){
+                        RemoveCases(i);
+                    }
+                }
+
+            }
+        }
+        for (int i = 3; i > 0; i--){
+            if(cases[cases.Count-i].transform.childCount != 0)
+                filledCases++;
+        }
+        if (filledCases == 3){
+            this.CreateTwoMoreCases();
+        }
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         if(StateManager.gameState == StateManager.GameState.TDTD){
@@ -38,6 +68,11 @@ public class DropContent : MonoBehaviour, IDropHandler
             go1.GetComponent<DropCase>().userStoryUI = eventData.pointerDrag.GetComponent<UserStoryUI>();
             go1.GetComponent<DropCase>().dropContent = this;
             go2.GetComponent<DropCase>().dropContent = this;
+            cases[cases.Count-1].nextDropCase = go1.GetComponent<DropCase>();
+            go1.GetComponent<DropCase>().previousDropCase = cases[cases.Count-1];
+            go1.GetComponent<DropCase>().nextDropCase = go2.GetComponent<DropCase>();
+            go2.GetComponent<DropCase>().previousDropCase = go1.GetComponent<DropCase>();
+            go2.GetComponent<DropCase>().nextDropCase = null;
             cases.Add(go1.GetComponent<DropCase>());
             cases.Add(go2.GetComponent<DropCase>());
             go1.GetComponent<DropCase>().OnDrop(eventData);
@@ -47,8 +82,6 @@ public class DropContent : MonoBehaviour, IDropHandler
         foreach (Transform child in content){
             if (child.childCount == 0){
                 child.GetComponent<DropCase>().AddUsUI(userStoryUI);
-                if (type == UserStory.State.PRODUCT_BACKLOG){
-                }
                 return;
             }
         }
@@ -62,6 +95,11 @@ public class DropContent : MonoBehaviour, IDropHandler
         go2.GetComponent<DropCase>().dropContent = this;
         go1.transform.localScale = Vector3.one;
         go2.transform.localScale = Vector3.one;
+        cases[cases.Count-1].nextDropCase = go1.GetComponent<DropCase>();
+        go1.GetComponent<DropCase>().previousDropCase = cases[cases.Count-1];
+        go1.GetComponent<DropCase>().nextDropCase = go2.GetComponent<DropCase>();
+        go2.GetComponent<DropCase>().previousDropCase = go1.GetComponent<DropCase>();
+        go2.GetComponent<DropCase>().nextDropCase = null;
         cases.Add(go1.GetComponent<DropCase>());
         cases.Add(go2.GetComponent<DropCase>());
         go1.GetComponent<DropCase>().AddUsUI(userStoryUI);
@@ -87,5 +125,44 @@ public class DropContent : MonoBehaviour, IDropHandler
                 }
             }
         return userStories;
+    }
+
+    public void CreateTwoMoreCases(){
+        GameObject go1 = Instantiate(casePrefab);
+        GameObject go2 = Instantiate(casePrefab);
+        go1.transform.SetParent(content);
+        go2.transform.SetParent(content);
+        go1.AddComponent<DropCase>();
+        go2.AddComponent<DropCase>();
+        go1.GetComponent<DropCase>().dropContent = this;
+        go2.GetComponent<DropCase>().dropContent = this;
+        go1.transform.localScale = Vector3.one;
+        go2.transform.localScale = Vector3.one;
+        cases[cases.Count-1].nextDropCase = go1.GetComponent<DropCase>();
+        go1.GetComponent<DropCase>().previousDropCase = cases[cases.Count-1];
+        go1.GetComponent<DropCase>().nextDropCase = go2.GetComponent<DropCase>();
+        go2.GetComponent<DropCase>().previousDropCase = go1.GetComponent<DropCase>();
+        go2.GetComponent<DropCase>().nextDropCase = null;
+        cases.Add(go1.GetComponent<DropCase>());
+        cases.Add(go2.GetComponent<DropCase>());
+    }
+
+    public void RemoveCases(int index){
+        GameObject go1 = cases[index].gameObject;
+        GameObject go2 = cases[index+1].gameObject;
+        if(index + 2 >= cases.Count){
+            cases[index-1].nextDropCase = null;
+            cases.RemoveAt(index+1);
+            cases.RemoveAt(index);
+            Destroy(go1);
+            Destroy(go2);
+            return;
+        }
+        cases[index-1].nextDropCase = cases[index+2];
+        cases[index+2].previousDropCase = cases[index-1];
+        cases.RemoveAt(index+1);
+        cases.RemoveAt(index);
+        Destroy(go1);
+        Destroy(go2);
     }
 }
